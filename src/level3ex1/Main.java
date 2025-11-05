8921f9b8bed98581ff193c3bd4f08c504b4c7414
apm <apm@mail.com>
2025-11-05 12:48:32 +0100

    T1L3E1 Schedule for options Menu

--- src/level3ex1/Main.java
+++ src/level3ex1/Main.java
@@ -3,7 +3,7 @@ package level3ex1;
 import java.util.Scanner;
 
 public class Main {
-    static void printMenu(){
+    private static void printMenu(){
         System.out.println("Main Menu :");
         System.out.println("1.- Add Writer");
         System.out.println("2.- Delete Writer");
@@ -15,18 +15,79 @@ public class Main {
         System.out.println("8.- EXIT");
     }
 
-    static int getOption(){
+    private static int getOption(){
         Scanner scanner = new Scanner(System.in);
-        String readLine = "";
-        int option = 0;
+        String readLine;
+        int option;
 
         System.out.print("\nChoose option: ");
         readLine = scanner.nextLine();
         if (readLine.length() == 1 & (readLine.charAt(0) >'0' & readLine.charAt(0) <'9'))
             option = Integer.parseInt(readLine);
-        else
+        else {
             System.out.println("\nBad option!!");
+            option = 0;
+        }
         return option;
+    }
+
+// Start: optiona Methods
+    private static void addWriter() {
+        System.out.println("TODO addWriter!!!");
+    }
+
+    private static void deleteWriter() {
+        System.out.println("TODO deleteWriter!!!");
+    }
+
+    private static void assignNewsToWriter() {
+        System.out.println("TODO assignNewsToWriter!!!");
+    }
+
+    private static void deleteNews() {
+        System.out.println("TODO deleteNews!!!");
+    }
+
+    private static void showNewsByWriter() {
+        System.out.println("TODO showNewsByWriter!!!");
+    }
+
+    private static void calculateScoreNews() {
+        System.out.println("TODO calculateScoreNews!!!");
+    }
+
+    private static void calculateNewsPrice() {
+        System.out.println("TODO calculateScoreNews!!!");
+    }
+
+    // END: options  Methods
+    private static void executeOption(int option) {
+        switch (option) {
+            case 1:
+                addWriter();
+                break;
+            case 2:
+                deleteWriter();
+                break;
+            case 3:
+                assignNewsToWriter();
+                break;
+            case 4:
+                deleteNews();
+                break;
+            case 5:
+                showNewsByWriter();
+                break;
+            case 6:
+                calculateScoreNews();
+                break;
+            case 7:
+                calculateNewsPrice();
+                break;
+            default:
+                System.out.println("TODO ejecutar option!!!");
+        }
+
     }
 
     static void menu(){
@@ -37,12 +98,13 @@ public class Main {
             printMenu();
             option = getOption();
             if (option != 8)
-                System.out.println("TODO ejecutar option!!!");
+                executeOption(option);
             else
                 exit = true;
             System.out.println();
         }
     }
+
     public static void main(String[] args){
         menu();
     }
